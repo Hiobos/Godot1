50e211dbf88c81164ac1d928d6ae153e41fe7dfa
Hubert Mikielewicz <152910024+Hiobos@users.noreply.github.com>
2025-05-08 15:11:33 +0200

    added stuff:
    -changed map
    -added platforms
    -background
    -hitbox, after falling of map game resets
    -coins
    -animations

--- new-game-project/scenes/player.gd
+++ new-game-project/scenes/player.gd
@@ -11,14 +11,20 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = -300
+		
+	if Input.is_action_just_pressed("move_down") and not is_on_floor():
+		velocity.y = 300
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
-	if direction:
+	if direction and is_on_floor():
 		velocity.x = direction * SPEED
+	elif direction and not is_on_floor():
+		velocity.x = direction * (SPEED / 1.5)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
 
 	move_and_slide()
