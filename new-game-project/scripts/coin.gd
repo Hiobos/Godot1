a60a9fb08b19e2d9ba68d765205f1ac7c2700755
Hubert Mikielewicz <152910024+Hiobos@users.noreply.github.com>
2025-05-10 22:48:07 +0200

    Music
    Added music, sfx, and changed how coins disappear

--- new-game-project/scripts/coin.gd
+++ new-game-project/scripts/coin.gd
@@ -1,17 +1,9 @@
 extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+@onready var game_manager: Node = %GameManager
+@onready var pickup_animation: AnimationPlayer = $pickup_animation
 
 
 func _on_body_entered(body: Node2D) -> void:
-	print("+1 Coin")
-	queue_free()
-	pass # Replace with function body.
+	game_manager.add_point()
+	pickup_animation.play("pickup")
