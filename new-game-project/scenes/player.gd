eefc2f7909ef4a4f7a01a8fb6d4a93d50a22d1de
Hubert Mikielewicz <152910024+Hiobos@users.noreply.github.com>
2025-05-10 22:06:55 +0200

    multiple changes
    -sprite direction changes
    -animations change according to action
    -movement: wsad and arrows
    -labels
    -map changes
    -death improvement

--- new-game-project/scenes/player.gd
+++ new-game-project/scenes/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
@@ -10,7 +11,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -300
 		
 	if Input.is_action_just_pressed("move_down") and not is_on_floor():
@@ -19,12 +20,23 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
-	if direction and is_on_floor():
+	if direction:
 		velocity.x = direction * SPEED
-	elif direction and not is_on_floor():
-		velocity.x = direction * (SPEED / 1.5)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+
+	if direction == -1:
+		animated_sprite.flip_h = true
+	elif direction == 1:
+		animated_sprite.flip_h = false
 	
 
 	move_and_slide()
